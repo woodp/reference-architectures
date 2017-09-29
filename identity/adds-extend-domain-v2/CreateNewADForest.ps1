f73a4d3be0f33d4a6173c2bc1b52e78e98e073f4
Pedro Wood <mail_pedro@yahoo.com.ar>
2017-09-29 10:50:19 -0300

    create new ad forest test

---
+++ identity/adds-extend-domain-v2/CreateNewADForest.ps1
@@ -0,0 +1,110 @@
+# $DomainName         -  FQDN for the Active Directory Domain to create
+# $AdminCreds         -  a PSCredentials object that contains username and password 
+#                        that will be assigned to the Domain Administrator account
+# $SafeModeAdminCreds -  a PSCredentials object that contains the password that will
+#                        be assigned to the Safe Mode Administrator account
+# $myFirstUserCreds   -  a PSCredentials object that contains the username and
+#                        password for the first domain user account to create
+# $RetryCount         -  defines how many retries should be performed while waiting
+#                        for the domain to be provisioned
+# $RetryIntervalSec   -  defines the seconds between each retry to check if the 
+#                        domain has been provisioned 
+Configuration CreateNewADForest {
+param
+    #v1.4
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SafeModeAdminCreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$myFirstUserCreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xActiveDirectory, xNetworking, xPendingReboot
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    $Interface = Get-NetAdapter | Where-Object Name -Like “Ethernet*”|Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        } 
+
+        WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"
+        }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature RSAT
+        {
+            Ensure = "Present"
+            Name = "RSAT"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+        }  
+
+        xADDomain FirstDC 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "C:\NTDS"
+            LogPath = "C:\NTDS"
+            SysvolPath = "C:\SYSVOL"
+            DependsOn = "[WindowsFeature]ADDSInstall","[xDnsServerAddress]DnsServerAddress"
+        }
+
+        xWaitForADDomain DscForestWait
+        {
+            DomainName = $DomainName
+            DomainUserCredential = $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
+            DependsOn = "[xADDomain]FirstDC"
+        } 
+
+        xADUser FirstUser 
+        { 
+            DomainName = $DomainName 
+            DomainAdministratorCredential = $DomainCreds 
+            UserName = $myFirstUserCreds.Username 
+            Password = $myFirstUserCreds
+            Ensure = "Present" 
+            DependsOn = "[xWaitForADDomain]DscForestWait" 
+        } 
+
+        xPendingReboot Reboot1
+        { 
+            Name = "RebootServer"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+   }
+}
